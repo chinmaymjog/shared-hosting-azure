d4f1a39a301b0d3a8a9523ab748125590272e29d
Chinmay Jog <chinmaymjog@gmail.com>
2026-02-22 16:02:54 +0530

    fix(terraform): Add moved block for NSG rule to resolve state conflict

--- azure-lamp-hosting/terraform/modules/hub/main.tf
+++ azure-lamp-hosting/terraform/modules/hub/main.tf
@@ -64,6 +64,11 @@ resource "azurerm_network_security_rule" "ssh" {
   network_security_group_name  = azurerm_network_security_group.hub-nsg.name
 }
 
+moved {
+  from = azurerm_network_security_rule.jenkis
+  to   = azurerm_network_security_rule.semaphore
+}
+
 resource "azurerm_network_security_rule" "semaphore" {
   name                         = "SemaphoreAccess"
   priority                     = 102
